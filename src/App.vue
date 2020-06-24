8419a71c0784e522a7e7bf870f8585fde03a7fd7
JacobTyBruceLaptop <59230762+JacobTyBruce@users.noreply.github.com>
2020-06-24 07:20:24 -0400

    fixed app.vue and added menu start \

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <v-menu :offset-y="offset" :close-on-click="closeOnClick">
+      <template v-slot:activator="{ on }">
+        <button v-on="on">Click</button>
+      </template>
+    </v-menu>
+    <v-list v-for='(item, index) in items' v-bind:key="index">
+      {{item.title}}
+    </v-list>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  data: function() {
+    return {
+    items: [
+      {title: 1},
+      {title: 2},
+      {title: 3},
+      {title: 4}
+    ],
+    closeOnClick: true
+  }
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-menu>
+      <template v-slot:activator="{ on }">
+        <button v-on="on">Click</button>
+      </template>
+    </v-menu>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
