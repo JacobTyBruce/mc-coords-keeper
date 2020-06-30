11a5da6aff4ec5eb3ac1ccedd936788120c5fdf2
JacobTyBruce <jacobtybruce@gmail.com>
2020-06-30 00:42:53 -0400

    Started to add JSON config

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
   <v-app>
-    <v-menu :offset-y="offset" :close-on-click="closeOnClick">
-      <template v-slot:activator="{ on }">
-        <button v-on="on">Click</button>
-      </template>
-    </v-menu>
-    <v-list v-for='(item, index) in items' v-bind:key="index">
-      {{item.title}}
-    </v-list>
+    <Header />
+    <v-main>
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">Choose World</v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" three-line>
+              <v-list-item-content>
+              <v-list-item-title>{{item.title}}</v-list-item-title>
+              <v-list-item-subtitle>{{item.desc}}</v-list-item-subtitle>
+              <v-list-item-subtitle>
+                <v-btn :to="item.routeTo">View Coords</v-btn>
+              </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import * as data from "./assets/coords.json";
+console.log(data);
+const {name} = data;
 
 export default {
-  name: 'App',
+  name: "App",
+  components: { Header },
   data: function() {
     return {
-    items: [
-      {title: 1},
-      {title: 2},
-      {title: 3},
-      {title: 4}
-    ],
-    closeOnClick: true
-  }
+      items: [
+      { title: 1, desc: 'desc1', routeTo: '/world1' },
+      { title: 2, desc: 'desc2', routeTo: '/world2' },
+      { title: 3, desc: 'desc3', routeTo: '/world3' },
+      { title: 4, desc: 'desc4', routeTo: '/world4' },
+      ]
+    };
+  },
+  created: function() {
+    console.log(name)
   }
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-app-bar app dark>
+        hello
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
