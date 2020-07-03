2fda4e5dc1aab2915d783d3b8609177d999a3a5b
JacobTyBruce <jacobtybruce@gmail.com>
2020-07-02 23:23:19 -0400

    started work on WorldView

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
     <v-app-bar app dark>
-        hello
+        <!--<router-link tag='span' to='/'> -->
+        <v-avatar
+            size="50"
+        >
+            <img src="https://www.vhv.rs/dpng/d/439-4398043_bee-minecraft-piglin-hd-png-download.png" alt="alt">
+        </v-avatar>
+        <v-divider vertical inset></v-divider>
+        <h1> MCoordinate Keeper </h1>
+        <!-- </router-link> -->
     </v-app-bar>
 </template>
 
--- src/views/WorldView.vue
+++ src/views/WorldView.vue
@@ -1,11 +1,13 @@
 <template>
     <v-main>
+        
+        <v-btn color="success" to="/"><v-icon mr-2>mdi-arrow-left-bold-outline</v-icon>Back</v-btn>
         <v-container>
             <v-row>
                 <v-col>
                     <v-avatar
                         color="white"
-                        size="48"
+                        size="100"
                         tile
                     >
                         <img src="https://vignette.wikia.nocookie.net/minecraft/images/f/fe/GrassNew.png/revision/latest?cb=20190903234415" alt="alt">
